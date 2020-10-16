735e8b423c0412f47b15ef94495bbb400ba56f74
Sebastian Hörl <hoerl.sebastian@gmail.com>
2020-10-16 10:43:30 +0200

    Update travis

--- tests/setup.sh
+++ tests/setup.sh
@@ -8,16 +8,23 @@ source "$HOME/env/miniconda/etc/profile.d/conda.sh"
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda env create -f environment.yml
+rm miniconda.sh
 
 # Java
 wget https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u252b09.tar.gz -O java.tar.gz
 tar xf java.tar.gz
+rm java.tar.gz
 
 # Maven
 wget http://mirror.easyname.ch/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -O maven.tar.gz
 tar xf maven.tar.gz
+rm maven.tar.gz
 
 # Osmosis
 wget https://github.com/openstreetmap/osmosis/releases/download/0.48.2/osmosis-0.48.2.tgz -O osmosis.tgz
 mkdir $HOME/env/osmosis
 tar xf osmosis.tgz -C $HOME/env/osmosis
+rm osmosis.tgz
+
+# Done
+echo "done" > $HOME/env/done
