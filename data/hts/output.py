1abc93ef0827e1f933925627915d23a3917f846b
Sebastian Hörl <hoerl.sebastian@gmail.com>
2021-12-20 21:07:37 +0100

    Add output stages for SIRENE and HTS (#84)

---
+++ data/hts/output.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import Levenshtein
+import tqdm
+import numpy as np
+import geopandas as gpd
+
+"""
+Often, we need the HTS for additioinal analyses, for instance, for the calibration
+of mode shares. This stage makes it easy to extract the cleaned HTS from the
+pipeline.
+"""
+
+def configure(context):
+    context.stage("data.hts.selected")
+    
+    context.config("output_path")
+    context.config("output_prefix", "ile_de_france_")
+
+def execute(context):
+    df_households, df_persons, df_trips = context.stage("data.hts.selected")
+
+    df_households.to_file("%s/%shts_households.csv" % (
+        context.config("output_path"), context.config("output_prefix")
+    ), sep = ";")
+
+    df_persons.to_file("%s/%shts_persons.csv" % (
+        context.config("output_path"), context.config("output_prefix")
+    ), sep = ";")
+
+    df_trips.to_file("%s/%shts_trips.csv" % (
+        context.config("output_path"), context.config("output_prefix")
+    ), sep = ";")
