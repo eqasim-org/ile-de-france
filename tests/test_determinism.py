ff1735041d986569d3cbb4693062b111f5e4a723
Sebastian Hörl <hoerl.sebastian@gmail.com>
2021-12-20 20:56:50 +0100

    Add output prefix to all output files (#83)

--- tests/test_determinism.py
+++ tests/test_determinism.py
@@ -47,9 +47,9 @@ def _test_determinism(index, data_path, tmpdir):
     synpp.run(stages, config, working_directory = cache_path)
 
     REFERENCE_HASHES = {
-        "activities.csv":                   "dcf8e08e9f238c90bff0298048251dac",
-        "persons.csv":                      "ed87e2b6dfd2a9914d5fc7b2bf6d52d3",
-        "households.csv":                   "882ce7dc1a44403d12c5aa10709c0d5b",
+        "ile_de_france_activities.csv":                   "dcf8e08e9f238c90bff0298048251dac",
+        "ile_de_france_persons.csv":                      "ed87e2b6dfd2a9914d5fc7b2bf6d52d3",
+        "ile_de_france_households.csv":                   "882ce7dc1a44403d12c5aa10709c0d5b",
         #"ile_de_france_population.xml.gz":  "e1407f918cb92166ebf46ad769d8d085",
         #"ile_de_france_network.xml.gz":     "5f10ec295b49d2bb768451c812955794",
         "ile_de_france_households.xml.gz":  "cdbd6ed5b175328861f237dc58dee1ff",
