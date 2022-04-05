95d2bf0c8b5aa8f84b81fc15a09b407549ae8b13
Sebastian Hörl <hoerl.sebastian@gmail.com>
2022-04-05 12:26:41 +0200

    update

--- lead/factors.py
+++ lead/factors.py
@@ -10,10 +10,12 @@ def execute(context):
 
     # Find growth factor for the requested year
     df_growth = pd.read_excel("%s/projections_scenario_central.xls" % context.config("lead_path"))
-    df_growth = df_growth[["code_Departements"].isin(df_codes["departement_id"])]
+    df_growth = df_growth[df_growth["code_Departements"].isin(df_codes["departement_id"])]
     df_growth["factor_2022"] = df_growth["pop_2022"] / df_growth["pop_2015"]
     df_growth["factor_2030"] = df_growth["pop_2030"] / df_growth["pop_2022"]
     df_growth = df_growth.rename(columns = { "code_Departements": "departement_id" })
     df_growth = df_growth[["departement_id", "factor_2022", "factor_2030"]]
+
+    print(df_growth)
 
     return df_growth
