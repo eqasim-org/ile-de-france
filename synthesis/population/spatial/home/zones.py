f310c863514ff659675c85715a57ab8b0b2d12fb
Sebastian Hörl <hoerl.sebastian@gmail.com>
2023-04-21 16:17:30 +0200

    Resolve FutureWarning about inplace parameter in pandas.Categorical.add_categories

--- synthesis/population/spatial/home/zones.py
+++ synthesis/population/spatial/home/zones.py
@@ -35,9 +35,8 @@ def execute(context):
     df_municipalities = context.stage("data.spatial.municipalities").set_index("commune_id")
     df_municipalities["population"] = context.stage("data.spatial.population").groupby("commune_id")["population"].sum()
 
-    df_households["commune_id"].cat.add_categories(
-        sorted(set(df_municipalities.index.unique()) - set(df_households["commune_id"].cat.categories)),
-        inplace = True)
+    df_households["commune_id"] = df_households["commune_id"].cat.add_categories(
+        sorted(set(df_municipalities.index.unique()) - set(df_households["commune_id"].cat.categories)))
 
     departements = df_households[~f_has_commune]["departement_id"].unique()
 
@@ -62,9 +61,8 @@ def execute(context):
     df_iris = context.stage("data.spatial.iris").set_index("iris_id")
     df_iris["population"] = context.stage("data.spatial.population").set_index("iris_id")["population"]
 
-    df_households["iris_id"].cat.add_categories(
-        sorted(set(df_iris.index.unique()) - set(df_households["iris_id"].cat.categories)),
-        inplace = True)
+    df_households["iris_id"] = df_households["iris_id"].cat.add_categories(
+        sorted(set(df_iris.index.unique()) - set(df_households["iris_id"].cat.categories)))
 
     communes = df_households[~f_has_iris & f_has_commune]["commune_id"].unique()
 
