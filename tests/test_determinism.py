8081183a51bb21f3dbbd2ee3e9292f09efda7dca
Sebastian Hörl <hoerl.sebastian@gmail.com>
2023-04-26 18:08:00 +0200

    Update hashes

--- tests/test_determinism.py
+++ tests/test_determinism.py
@@ -71,10 +71,10 @@ def _test_determinism(index, data_path, tmpdir):
     }
 
     REFERENCE_GPKG_HASHES = {
-        "ile_de_france_activities.gpkg":    "4bf6eab1d6b8dd81eb97c427e9ecf8e0",
-        "ile_de_france_commutes.gpkg":      "545660fd499b2166a9ac6ebc856b312c",
-        "ile_de_france_homes.gpkg":         "f504bd5addb53c653b253e8ce43cec97",
-        "ile_de_france_trips.gpkg":         "6ad230921d12786c18477da62c49b972",
+        "ile_de_france_activities.gpkg":    "f9e519cb5665c314431bcd16bbb8b1b8",
+        "ile_de_france_commutes.gpkg":      "2e752795b7cd8e0cd4c8d32e736e455e",
+        "ile_de_france_homes.gpkg":         "6f028d84944df9c4ae9342a47a932074",
+        "ile_de_france_trips.gpkg":         "6b7a772aad4b994e5b04076fd7c88266",
     }
 
     generated_csv_hashes = {
