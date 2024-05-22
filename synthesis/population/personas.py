90afa60d1a5a238569990a6956e2c18ae7b04f3d
Sebastian Hörl <hoerl.sebastian@gmail.com>
2024-05-22 09:05:54 +0200

    change persona imputation to IPU

--- synthesis/population/personas.py
+++ synthesis/population/personas.py
@@ -13,10 +13,9 @@ def configure(context):
     context.config("personas.scenario", "none")
 
 def execute(context):
-    df_census = context.stage("data.census.filtered").sort_values(by = "household_id").copy()
-    
-    scenario = context.config("personas.scenario")
+    df_census = context.stage("data.census.cleaned")
 
+    scenario = context.config("personas.scenario")
     if scenario == "none":
         return df_census
 
@@ -32,37 +31,69 @@ def execute(context):
     })
     assert len(df_scenario) > 0
 
-    # Perform weighting
-    df_households = df_census.groupby(["household_id", "persona"]).size().reset_index(name = "members")
-    df_households = pd.merge(df_households, df_census[["household_id", "weight"]].drop_duplicates(
-        "household_id", keep = "first"), on = "household_id")
+    # Prepare indexing
+    df_households = df_census[["household_id", "household_size", "weight"]].drop_duplicates("household_id")
+    df_households["household_index"] = np.arange(len(df_households))
+    df_census = pd.merge(df_census, df_households[["household_id", "household_index"]])
 
-    threshold = 1e-6
-    print("Weighting households for personas ...")
+    # Obtain weights and sizes as arrays
+    household_weights = df_households["weight"].values
+    household_sizes = df_households["household_size"].values
 
-    for iteration in range(0, 100):
-        differences = []
+    # Obtain the attribute levels and membership of attributes for all households
+    attribute_membership = []
+    attribute_counts = []
+    attribute_targets = []
 
-        for __, row in df_scenario.iterrows():
-            f_persona = df_households["persona"] == row["persona"]
-            df_selection = df_households[f_persona]
+    # Process personas
+    for persona, target_share in df_scenario.set_index("persona")["scenario_share"]:
+        f = df_census["persona"] == persona
 
-            total_count = np.sum(df_households["members"] * df_households["weight"])
-            persona_count = np.sum(df_selection["members"] * df_selection["weight"])
+        df_counts = df_census.loc[f, "household_index"].value_counts()
+        
+        attribute_targets.append(target_share * df_census["weight"].sum())
+        attribute_membership.append(df_counts.index.values)
+        attribute_targets.append(df_counts.values)
+    
+    # Process total
+    attribute_targets.append(df_census["weight"].sum())
+    attribute_membership.append(np.arange(len(household_sizes))) # all
+    attribute_counts.append(household_sizes)
 
-            current_weight = persona_count / total_count
-            target_weight = row["scenario_share"]
-            differences.append(current_weight - target_weight)
+    # Perform IPU to obtain update weights
+    update = np.ones((len(df_households),))
 
-            f_household = df_households["household_id"].isin(df_selection["household_id"])
-            df_households.loc[f_household, "weight"] *= target_weight / current_weight
+    minimum_factors = []
+    maximum_factors = []
 
-        mismatch = np.sum(np.abs(differences))
-        print("  Mismatch after iteration {}:".format(iteration), mismatch)
+    for iteration in range(100):
+        factors = []    
+        for k in np.arange(len(attribute_targets)):
+            selection = attribute_membership[k]
+        
+            target = attribute_targets[k]
+            current = np.sum(update[selection] * household_weights[selection] * attribute_counts[k])
+        
+            factor = target / current
+            factors.append(factor)
+                
+            update[selection] *= factor
 
-        if mismatch < threshold:
+        minimum_factors.append(np.min(factors))
+        maximum_factors.append(np.max(factors))
+
+        print(
+            "IPU", "it={}".format(iteration), 
+            "min={}".format(np.min(factors)),
+            "max={}".format(np.max(factors))
+        )
+
+        if np.max(factors) - np.min(factors) < 1e-3:
             break
 
+    # Update the weights
+    df_households["weight"] *= update
+    
     # Re-apply weights
     df_census = df_census.drop(columns = "weight")
     df_census = pd.merge(df_census, df_households[[
