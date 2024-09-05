244dbbe409daaef66dc512a02ad385c7ba36b055
Sebastian Hörl <sebastian.horl@irt-systemx.fr>
2024-09-05 19:16:43 +0200

    set final eqasim-java commit

--- matsim/runtime/eqasim.py
+++ matsim/runtime/eqasim.py
@@ -6,8 +6,8 @@ import matsim.runtime.java as java
 import matsim.runtime.maven as maven
 
 DEFAULT_EQASIM_VERSION = "1.5.0"
-DEFAULT_EQASIM_BRANCH = "feat/vehicles"
-DEFAULT_EQASIM_COMMIT = "4728f66"
+DEFAULT_EQASIM_BRANCH = "develop"
+DEFAULT_EQASIM_COMMIT = "ece4932"
 
 def configure(context):
     context.stage("matsim.runtime.git")
