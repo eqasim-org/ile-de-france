0a0b12af3966cd54f4f39dc06f92f43dbbbe6aef
Sebastian Hörl <hoerl.sebastian@gmail.com>
2026-08-12 07:56:29 +0200

    fix: selection of departments in sirene

--- data/sirene/raw_siret.py
+++ data/sirene/raw_siret.py
@@ -26,10 +26,10 @@ def execute(context):
     deps2 = {dep for dep in requested_departements if len(dep) == 2}
     deps3 = {dep for dep in requested_departements if len(dep) == 3}
     assert len(deps2) + len(deps3) == len(requested_departements)
-    if deps2:
-        lf = lf.filter(pl.col("codeCommuneEtablissement").str.slice(0, 2).is_in(deps2))
-    if deps3:
-        lf = lf.filter(pl.col("codeCommuneEtablissement").str.slice(0, 3).is_in(deps3))
+    lf = lf.filter(
+        pl.col("codeCommuneEtablissement").str.slice(0, 2).is_in(deps2) |
+        pl.col("codeCommuneEtablissement").str.slice(0, 3).is_in(deps3)
+    )
 
     df_siret = lf.select(
         "siren",
