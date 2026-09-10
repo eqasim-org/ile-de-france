c611798b2affc8539c470c7926479926ddd100c6
Sebastian Hörl <hoerl.sebastian@gmail.com>
2026-09-10 14:25:23 +0200

    fix: relax excess municipalities check

--- data/census/filtered.py
+++ data/census/filtered.py
@@ -21,11 +21,11 @@ def execute(context):
     df = df[df["departement_id"].isin(requested_departements)]
 
     excess_communes = set(df["commune_id"].unique()) - set(df_codes["commune_id"].unique())
-    if not excess_communes == {"undefined"}:
+    if len(excess_communes) > 0 and not excess_communes == {"undefined"}:
         raise RuntimeError("Found additional communes: %s" % excess_communes)
 
     excess_iris = set(df["iris_id"].unique()) - set(df_codes["iris_id"].unique())
-    if not excess_iris == {"undefined"}:
+    if len(excess_iris) > 0 and not excess_iris == {"undefined"}:
         raise RuntimeError("Found additional IRIS: %s" % excess_iris)
 
     return df
